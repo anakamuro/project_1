5966f12b491245702c118f77baced196ac4e57b1
Akihiko Nakamura <anakamuro@earthlink.net>
2023-01-05 15:27:05 -0500

    commit

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
+const cardModel = document.createElement('div');
+cardModel.classList.add('card');
+const dealer = document.getElementById("dealer")
+const player = document.getElementById("player")
+const hitButton = document.getElementById("hit-button")
+const passButton = document.getElementById("pass-button")
+const resetButton = document.getElementById("reset-button")
+const buttonContainer = document.getElementById("button-container")
+const notice = document.getElementById("notice")
+const nextHandButton = document.getElementById("next-hand-button")
+const betButton = document.querySelector('.bet-amount');
+const inputBet = document.querySelector('input')
+const player1 = document.querySelector('.player1');
+const dealer1 = document.querySelector('.dealer1');
+const result = document.querySelector('.result');
+let playerCash = document.querySelector('.player-cash')
+playerCash.textContent = "Player Cash $100";
+let scoreBoard = document.querySelector('.score-board');
+//scoreBoard.textContent = "Dealer 0 vs Player 0";
+scoreBoard.style.fontSize = "2em";
 
-let allDecks = []
-let dealerHand = []
-let playerHand = []
-const CARD_MODEL = document.createElement('div');
-CARD_MODEL.classList.add('card');
-const DEALER = document.getElementById("dealer")
-const PLAYER = document.getElementById("player")
-const HIT_BUTTON = document.getElementById("hit-button")
-const PASS_BUTTON = document.getElementById("pass-button")
-const RESET_BUTTON = document.getElementById("reset-button")
-const BUTTON_CONTAINER = document.getElementById("button-container")
-const NOTICE = document.getElementById("notice")
-const NEX_HAND_BUTTON = document.getElementById("next-hand-button")
-
-   const betButton = document.querySelector('.bet-amount');
-
-  const inputBet = document.querySelector('input')
-  
 /*
   game.status = document.createElement('div')
   game.status.classList.add('message')
@@ -25,72 +27,38 @@ const NEX_HAND_BUTTON = document.getElementById("next-hand-button")
   game.status.style.height = "2em";
   game.append(game.status)
 */
-  let cash = 100;
-  let bet = 0;
+let allDecks = []
+let dealerHand = []
+let playerHand = []
+let cash = 100;
+let bet = 0;
+const score = [0, 0];
+let testDeck = makeDeck()
+shuffle(testDeck)
+console.log(testDeck)
 
-  game.playerCash = document.createElement('div');
-  game.playerCash.classList.add('message');
-  game.playerCash.textContent = "Player Cash $100";
-  game.append(game.playerCash)
 
-  updateCash();
+
 
 function init() {
-  game.main = document.querySelector('#game');
-
-  game.dashboard = document.createElement('div')
- 
-  
-  game.scoreboard = document.createElement('div')
-  game.status = document.createElement('div')
-  game.scoreboard.textContent = "Dealer 0 vs Player 0";
-  game.scoreboard.style.fontSize = "2em";
-  game.append(game.scoreboard)
-  game.main.append(game.scoreboard)
-  return {
-    init: init
-  }
+	game.main = document.querySelector('#game');
+	game.dashboard = document.createElement('div')
+	game.status = document.createElement('div')
 }
 
-function updateCash(){
-console.log(isNaN(inputBet.value));
-if(isNaN(inputBet.value) || (inputBet.value.length < 1)){
-  inputBet.value = 0;
-}
-if(inputBet.value > cash){
-  inputBet.value = cash
-}
-bet = Number(inputBet.value)
-game.playerCash.textContent = "Player Cash $"+ (cash - bet)
-}
-game.playerCash.textContent = "Player Cash $"+ (cash - bet)
-/*
-function lockWager(tog){
-  game.inputBet.disabled = tog;
-  game.betButton.disabled = tog;
-}
-*/
-function setBet(){
-  const status = document.querySelector('.message')
-  status.textContent = "You bet $"+bet;
-  cash = cash - bet;
-  game.playerCash.textContent = "Player Cash $"+cash;
- // lockWager(true);
- console.log('hello')
-}
 
 function makeDeck(){
-const suits = ["H", "C", "D", "S"];
-const nums = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
-let deck = []
+	const suits = ["H", "C", "D", "S"];
+	const nums = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+	let deck = []
 
 
-for( let suitCounter = 0; suitCounter < 4; suitCounter++){
-    for( let numCounter = 0; numCounter < 13; numCounter++){
-        deck.push(nums[numCounter] + suits[suitCounter])
-    }
-}
-return deck;
+	for( let suitCounter = 0; suitCounter < 4; suitCounter++){
+		for( let numCounter = 0; numCounter < 13; numCounter++){
+			deck.push(nums[numCounter] + suits[suitCounter])
+		}
+	}
+	return deck;
 }
 
 
@@ -103,133 +71,191 @@ function shuffle(deck) {
     }
 }
 
- let testDeck = makeDeck()
- shuffle(testDeck)
- console.log(testDeck)
+ 
  
 const selectRandomCard = () => {
-  const randomIndex = Math.floor(Math.random()* 52)
-  const newDeck = makeDeck()
-  allDecks = [ ...allDecks, ...newDeck]
-  const card = allDecks[randomIndex]
-  allDecks.splice(randomIndex, 1)
-  return card;
+	const randomIndex = Math.floor(Math.random()* 52)
+	const newDeck = makeDeck()
+	allDecks = [ ...allDecks, ...newDeck]
+	const card = allDecks[randomIndex]
+	allDecks.splice(randomIndex, 1)
+	return card;
 }
+/*
 const randomCard = selectRandomCard()
-
+*/
  
+function Score(){
+	scoreBoard.textContent = `Dealer ${score[0]} vs Player ${score[1]}`
+}
+Score()
+
 const dealHands = () => {
-    dealerHand = [selectRandomCard(), selectRandomCard()]
+	console.log(testDeck)
+    dealerHand = [testDeck.pop(), testDeck.pop()]
+	console.log(dealerHand)
+	console.log(testDeck)
+	playerHand = [testDeck.pop(), testDeck.pop()]
+	console.log(playerHand)
+	console.log(testDeck)
+	
     dealerHand.forEach((card, index)=>{
-      const newCard = CARD_MODEL.cloneNode(true);
+      const newCard = cardModel.cloneNode(true);
       index === 0 ? newCard.classList.add('back') : newCard.innerHTML = card;
       (card[card.length -1] === "D" || card[card.length - 1] === "H") && newCard.setAttribute('data-red', true)
-      DEALER.append(newCard);
+      dealer.append(newCard);
     })
-    playerHand = [selectRandomCard(), selectRandomCard()]
     playerHand.forEach((card)=>{
-      const newCard = CARD_MODEL.cloneNode(true);
+      const newCard = cardModel.cloneNode(true);
       newCard.innerHTML = card;
       (card[card.length -1] === "D" || card[card.length - 1] === "H") && newCard.setAttribute('data-red', true)
-      PLAYER.append(newCard);
+      player.append(newCard);
     })
 }
 
-const calcValue = (hand) => {
-  let value = 0;
-  let hasAce = 0;
-  hand.forEach((card) => {
-    if(card.length === 2){
-      if(card[0] === 'A'){
-        hasAce += 1
-      } else {
-        (card[0] === 'K' || card[0] === "Q" || card[0] === "J")? value+=10: value += Number(card[0])
-      }
-    } else {
-      value+= 10
-    }
-  })
+//dealHands()
 
-  if(hasAce > 0){
-   value + 11 > 21 ? value +=1 : value += 11;
-   value += (hasAce-1)+1;
-  }
-  return value
+const calcValue = (hand) => {
+	let value = 0;
+	let hasAce = 0;
+	hand.forEach((card) => {
+		if(card.length === 2){
+		if(card[0] === 'A'){
+			hasAce += 1
+		} else {
+			(card[0] === 'K' || card[0] === "Q" || card[0] === "J")? value+=10: value += Number(card[0])
+		}
+		} else {
+		value+= 10
+		}
+	})
+
+	if(hasAce > 0){
+	value + 11 > 21 ? value +=1 : value += 11;
+	value += (hasAce-1)+1;
+	}
+	return value
 }
 
 const hitPlayer = () => {
-  const newCard = selectRandomCard()
-  playerHand.push(newCard);
-  const newCardNode = CARD_MODEL.cloneNode(true);
-  newCardNode.innerHTML = newCard;
-  PLAYER.append(newCardNode)
-  const handValue = calcValue (playerHand);
-  if(handValue > 21){
-    console.log("bust")
-    alert("bust")
-  } 
+	const newCard = selectRandomCard()
+	playerHand.push(newCard);
+	const newCardNode = cardModel.cloneNode(true);
+	newCardNode.innerHTML = newCard;
+	player.append(newCardNode)
+	const handValue = calcValue(playerHand);
+	if(handValue > 21){
+		player1.textContent = "Player busts. Dealer wins"
+		score[0]++;
+	} 
 }
 
-const decideWinneer = async() => {
-  let dealerValue = await calcValue(dealerHand)
-  let playerValue = await calcValue(playerHand)
+const decideWinneer = () => {
+	let dealerValue = calcValue(dealerHand)
+	let playerValue = calcValue(playerHand)
 
- alert(`Dealer has ${dealerValue}, you have ${playerValue}`)
- if(dealerValue === playerValue){
-  cash = cash + bet
-  alert("dealer and player are tie.")
-} else if ( dealerValue > playerValue){
-   alert("dealer wins!")
-} else {
-  alert("player wins")
-  cash = cash + (bet * 2)
-}
+	result.textContent = (`Dealer has ${dealerValue}, you have ${playerValue}`)
+	if(dealerValue === playerValue){
+	cash = cash + bet
+	player1.textContent = "dealer and players are tie."
+	} else if ( dealerValue > playerValue){
+	dealer1.textContent = "dealer wins";
+    score[0]++;
+	} else if ( playerValue === 21){
+	player1.textContent = "player has 21. BlackJack!!!";
+	score[1]++;
+	} else {
+	player1.textContent = "player wins."
+	cash = cash + (bet * 2)
+	score[1]++;
+	}
+	if(cash < 1){
+		cash = 0;
+		bet = 0;
+	}
 }
 
-if(cash < 1){
-  cash = 0;
-  bet = 0;
-}
+
 const hitDealer = async() => {
-  const hiddenCard = DEALER.children[0];
-  hiddenCard.classList.remove("back");
-  hiddenCard.innerHTML = dealerHand[0];
-  let handValue = await calcValue(dealerHand);
-  if(handValue < 16){
-    let newCard = selectRandomCard();
-    dealerHand.push(newCard)
-    const newCardNode = CARD_MODEL.cloneNode(true);
-    newCardNode.innerHTML = newCard;
-    DEALER.append(newCardNode)
-    handValue = await calcValue(dealerHand)
-  }
-  if(handValue < 16 ){
-    hitDealer();
-  } else if (handValue === 21){
-    alert("dealer has 21!")
-  } else if (handValue > 21){
-    alert("dealer bust")
-  } else {
-    decideWinneer()
-  }
+	const hiddenCard = dealer.children[0];
+	hiddenCard.classList.remove("back");
+	hiddenCard.innerHTML = dealerHand[0];
+	let handValue = await calcValue(dealerHand);
+	if(handValue < 16){
+		let newCard = selectRandomCard();
+		dealerHand.push(newCard)
+		const newCardNode = cardModel.cloneNode(true);
+		newCardNode.innerHTML = newCard;
+		dealer.append(newCardNode)
+		handValue = await calcValue(dealerHand)
+	}
+	if(handValue < 16 ){
+		hitDealer();
+	} else if (handValue === 21){
+        dealer1.textContent = "Dealer got 21. BlackJack!!! Dealer won."
+		score[0]++;
+	} else if (handValue > 21){
+		dealer1.textContent = "Dealer Bust. Player won."
+		score[1]++;
+	} else {
+		decideWinneer()
+	}
 }
 
- function reset(){
-  allDecks = 0;
-  dealerHand = 0;
-  playerHand = 0;
-  deck = 0;
-  cash = cash
- }
+function updateCash(){
+	console.log(isNaN(inputBet.value));
+	if(isNaN(inputBet.value) || (inputBet.value.length < 1)){
+		inputBet.value = 0;
+	}
+	if(inputBet.value > cash){
+		inputBet.value = cash
+	}
+	bet = Number(inputBet.value)
+	playerCash.textContent = "Player Cash $"+ (cash - bet)
+}
+/*
+function lockWager(tog){
+	game.inputBet.disabled = tog;
+	game.betButton.disabled = tog;
+}
+*/
+function setBet(){
+    const status = document.querySelector('.message')
+    status.textContent = "You bet $"+bet;
+    cash = cash - bet;
+    game.playerCash.textContent = "Player Cash $"+cash;
+    // lockWager(true);
+    console.log('hello')
+}
+  
 
- document.addEventListener('DOMContentLoaded', init)
-HIT_BUTTON.addEventListener('click', hitPlayer)
-PASS_BUTTON.addEventListener('click', hitDealer)
-RESET_BUTTON.addEventListener('click', reset)
+function resetCards(){
+	dealerHand = []
+	playerHand = []
+	while (dealer.hasChildNodes()){
+		console.log(dealer.childNodes)
+		dealer.removeChild(dealer.firstElementChild)
+	}
+	while (player.hasChildNodes()){
+		console.log(player.childNodes)
+		player.removeChild(player.firstElementChild)
+	}
+	player1.innerHTML = ""
+	dealer1.innerHTML = ""
+	result.innerHTML = ""
+	testDeck = makeDeck()
+    shuffle(testDeck)
+	dealHands()
+}
+
+document.addEventListener('DOMContentLoaded', init)
+hitButton.addEventListener('click', hitPlayer)
+passButton.addEventListener('click', hitDealer)
+resetButton.addEventListener('click', resetCards)
 dealHands()
 betButton.addEventListener('click', setBet)
 inputBet.addEventListener('change', updateCash)
-
+/*
 RESET_BUTTON.addEventListener("click", function (){
-    location.reload();
-  })
+  
+  })*/
